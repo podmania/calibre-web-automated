6bde031e28b022bb643b59ea2f3dfe7814192e28
Bryce Hoehn <bryce.hoehn@icloud.com>
2026-04-26 00:14:56 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,75 +1,21 @@
 {
-  description = "Calibre-Web-Automated distroless image";
-
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
   outputs = { self, nixpkgs }: let
-    system = builtins.currentSystem;
-    pkgs = nixpkgs.legacyPackages.${system};
-
-    # ---- CI updates these lines ----
-    cwaRev = "v4.0.6";
-    cwaSha256 = "0y3a7w0lcqlslc4l2ygnbkn9c4gva4fbkmmqg1rdigwjr33c86z0";
-    # --------------------------------
-
-    src = pkgs.fetchFromGitHub {
-      owner = "crocodilestick";
-      repo = "Calibre-Web-Automated";
-      rev = cwaRev;
-      sha256 = cwaSha256;
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ (self: super: { calibre-web-automated = super.callPackage ./package.nix { }; }) ];
     };
   in {
     packages.${system}.calibre-web-automated = pkgs.dockerTools.buildLayeredImage {
       name = "calibre-web-automated";
-      tag = "latest";
-      contents = [
-        (pkgs.stdenv.mkDerivation {
-          name = "cwa-app";
-          __noChroot = true;
-          nativeBuildInputs = with pkgs; [ python3 pkg-config openldap cyrus_sasl cacert ];
-          buildCommand = ''
-            set -e
-            mkdir -p $out/app
-            cp -r ${src}/* $out/app/
-
-            export PKG_CONFIG_PATH=${pkgs.openldap}/lib/pkgconfig:${pkgs.cyrus_sasl}/lib/pkgconfig
-
-            python3 -m venv $out/venv
-            source $out/venv/bin/activate
-
-            pip install --no-cache-dir -r $out/app/requirements.txt
-            pip install --no-cache-dir -r $out/app/optional-requirements.txt
-
-            SITE_PACKAGES=$(find $out/venv/lib -type d -name site-packages | head -1)
-            echo "$out/app" > $SITE_PACKAGES/cwa.pth
-
-            # Clean up
-            rm -rf $out/venv/lib/python*/site-packages/pip*
-            rm -rf $out/venv/lib/python*/site-packages/setuptools*
-          '';
-        })
-        pkgs.calibre
-        pkgs.kepubify
-        pkgs.cacert
-        pkgs.tzdata
-        pkgs.openldap
-        pkgs.cyrus_sasl
-        pkgs.libffi
-        pkgs.openssl
-      ];
+      contents = [ pkgs.calibre-web-automated pkgs.calibre pkgs.kepubify pkgs.cacert pkgs.tzdata ];
       config = {
-        Env = [
-          "PORT=8083"
-          "SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt"
-          "PYTHONPATH=/app"
-        ];
+        Cmd = [ "${pkgs.calibre-web-automated}/bin/cps" ];
         ExposedPorts = { "8083/tcp" = {}; };
         Volumes = { "/config" = {}; "/books" = {}; };
-        Cmd = [ "/venv/bin/python" "/app/cps.py" ];
         User = "1000";
-        WorkingDir = "/config";
       };
     };
-    calibreWebAutomatedVersion = cwaRev;
   };
 }
