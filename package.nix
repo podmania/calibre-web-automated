7403e624da1a764815aeee395adaa7b62d02a8f7
Bryce Hoehn <bryce.hoehn@icloud.com>
2026-04-26 00:14:35 -0400

    Create package.nix

---
+++ package.nix
@@ -0,0 +1,71 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, python3Packages
+}:
+
+python3Packages.buildPythonApplication rec {
+  pname = "calibre-web-automated";
+  version = "4.0.6";        # CI updates this
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "crocodilestick";
+    repo = "Calibre-Web-Automated";
+    tag = version;
+    hash = "";              # CI replaces this
+  };
+
+  dependencies = with python3Packages; [
+    apscheduler
+    babel
+    bleach
+    chardet
+    cryptography
+    flask
+    flask-babel
+    flask-httpauth
+    flask-limiter
+    flask-principal
+    flask-wtf
+    iso-639
+    lxml
+    netifaces-plus
+    pycountry
+    pypdf
+    python-magic
+    pytz
+    regex
+    requests
+    sqlalchemy
+    tornado
+    unidecode
+    urllib3
+    wand
+  ];
+
+  optional-dependencies = {
+    comics = with python3Packages; [ comicapi natsort ];
+    gdrive = with python3Packages; [ gevent google-api-python-client greenlet httplib2 oauth2client pyasn1-modules pyyaml rsa uritemplate ];
+    gmail = with python3Packages; [ google-api-python-client google-auth-oauthlib ];
+    kobo = with python3Packages; [ jsonschema ];
+    ldap = with python3Packages; [ flask-simpleldap python-ldap ];
+    metadata = with python3Packages; [ faust-cchardet html2text markdown2 mutagen py7zr pycountry python-dateutil rarfile scholarly ];
+    oauth = with python3Packages; [ flask-dance sqlalchemy-utils ];
+  };
+
+  pythonRelaxDeps = [
+    "apscheduler" "bleach" "cryptography" "flask" "flask-limiter"
+    "lxml" "pypdf" "regex" "tornado" "unidecode" "wand"
+  ];
+
+  pythonImportsCheck = [ "cps" ];
+
+  meta = {
+    description = "Calibre-Web but Automated and with tons of New Features! Fully automate and simplify your eBook set up!";
+    homepage = "https://github.com/crocodilestick/Calibre-Web-Automated";
+    license = lib.licenses.gpl3Plus;
+    mainProgram = "cps";
+    platforms = lib.platforms.linux;
+  };
+}
