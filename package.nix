f45c274e7555d70a1d6aa4f200ffb372782a236f
Bryce Hoehn <bryce.hoehn@icloud.com>
2026-04-26 01:01:22 -0400

    Update package.nix

--- package.nix
+++ package.nix
@@ -15,6 +15,8 @@ python3Packages.buildPythonApplication rec {
     rev = "v${version}";
     hash = "sha256-4BvExsiSv9hyeLjWuRxR+xGW7Fz2eUEJo5piRgE/ang=";              # CI replaces this
   };
+  
+  packages = [ "cps" ];
 
   dependencies = with python3Packages; [
     apscheduler
